822f1a7eea80d6721851bf0002f3dd5fd3b597e8
André Mendes <andre.marcondes@betalearning.com.br>
2021-02-06 19:53:46 -0300

    gerando pagina de visualização de treinos

---
+++ AulaRemotaThriboCrossfit/AulaRemotaThriboCrossfit/Controllers/VerTreinoController.cs
@@ -0,0 +1,30 @@
+﻿using AulaRemotaThriboCrossfit.Data.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AulaRemotaThriboCrossfit.Controllers
+{
+    public class VerTreinoController : Controller
+    {
+        private readonly ITreinoRepository _treinoRepository;
+
+        public VerTreinoController(ITreinoRepository treinoRepository)
+        {
+            _treinoRepository = treinoRepository;
+        }
+
+        public async Task<IActionResult> Index(string id)
+        {
+            if (id != null)
+            {
+                var treino = await _treinoRepository.GetById(id);
+
+                if (treino != null)
+                {
+                    return View(treino);
+                }
+            }
+            return View("TreinoNaoDisponivel");
+        }
+    }
+}
